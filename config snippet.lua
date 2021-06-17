a50007b16736bdbfc6dba05637fc74af217f2090
timginter <gaertner.t@gmail.com>
2021-06-17 21:09:14 +0100

    add config code snippet

---
+++ config snippet.lua
@@ -0,0 +1,175 @@
+------------------------------------------------------------------------------------------
+-- AUTOSCROLL WITH MOUSE WHEEL BUTTON
+-- timginter @ GitHub
+------------------------------------------------------------------------------------------
+
+-- id of mouse wheel button
+local mouseScrollButtonId = 2
+
+-- scroll speed and direction config
+local scrollSpeedMultiplier = 0.15
+local scrollSpeedSquareAcceleration = false
+local reverseVerticalScrollDirection = false
+local mouseScrollTimerDelay = 0.01
+
+-- circle config
+local mouseScrollCircleRad = 10
+local mouseScrollCircleDeadZone = 5
+
+------------------------------------------------------------------------------------------
+
+local mouseScrollCircle = nil
+local mouseScrollTimer = nil
+local mouseScrollStartPos = 0
+local mouseScrollDragPosX = nil
+local mouseScrollDragPosY = nil
+
+overrideScrollMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    -- uncomment line below to see the ID of pressed button
+    --print(e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']))
+
+    if e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']) == mouseScrollButtonId then
+        -- remove circle if exists
+        if mouseScrollCircle then
+            mouseScrollCircle:delete()
+            mouseScrollCircle = nil
+        end
+
+        -- stop timer if running
+        if mouseScrollTimer then
+            mouseScrollTimer:stop()
+            mouseScrollTimer = nil
+        end
+
+        -- save mouse coordinates
+        mouseScrollStartPos = hs.mouse.getAbsolutePosition()
+        mouseScrollDragPosX = mouseScrollStartPos.x
+        mouseScrollDragPosY = mouseScrollStartPos.y
+
+        -- start scroll timer
+        mouseScrollTimer = hs.timer.doAfter(mouseScrollTimerDelay, mouseScrollTimerFunction)
+
+        -- don't send scroll button down event
+        return true
+    end
+end)
+
+overrideScrollMouseUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
+    if e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber']) == mouseScrollButtonId then
+        -- send original button up event if released within 'mouseScrollCircleDeadZone' pixels of original position and scroll circle doesn't exist
+        mouseScrollPos = hs.mouse.getAbsolutePosition()
+        xDiff = math.abs(mouseScrollPos.x - mouseScrollStartPos.x)
+        yDiff = math.abs(mouseScrollPos.y - mouseScrollStartPos.y)
+        if (xDiff < mouseScrollCircleDeadZone and yDiff < mouseScrollCircleDeadZone) and not mouseScrollCircle then
+            -- disable scroll mouse override
+            overrideScrollMouseDown:stop()
+            overrideScrollMouseUp:stop()
+
+            -- send scroll mouse click
+            hs.eventtap.otherClick(e:location(), mouseScrollButtonId)
+
+            -- re-enable scroll mouse override
+            overrideScrollMouseDown:start()
+            overrideScrollMouseUp:start()
+        end
+
+        -- remove circle if exists
+        if mouseScrollCircle then
+            mouseScrollCircle:delete()
+            mouseScrollCircle = nil
+        end
+
+        -- stop timer if running
+        if mouseScrollTimer then
+            mouseScrollTimer:stop()
+            mouseScrollTimer = nil
+        end
+
+        -- don't send scroll button up event
+        return true
+    end
+end)
+
+overrideScrollMouseDrag = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
+    -- sanity check
+    if mouseScrollDragPosX == nil or mouseScrollDragPosY == nil then
+        return true
+    end
+
+    -- update mouse coordinates
+    mouseScrollDragPosX = mouseScrollDragPosX + e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+    mouseScrollDragPosY = mouseScrollDragPosY + e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+
+    -- don't send scroll button drag event
+    return true
+end)
+
+function mouseScrollTimerFunction()
+    -- sanity check
+    if mouseScrollDragPosX ~= nil and mouseScrollDragPosY ~= nil then
+        -- get cursor position difference from original click
+        xDiff = math.abs(mouseScrollDragPosX - mouseScrollStartPos.x)
+        yDiff = math.abs(mouseScrollDragPosY - mouseScrollStartPos.y)
+
+        -- draw circle if not yet drawn and cursor moved more than 'mouseScrollCircleDeadZone' pixels
+        if mouseScrollCircle == nil and (xDiff > mouseScrollCircleDeadZone or yDiff > mouseScrollCircleDeadZone) then
+            mouseScrollCircle = hs.drawing.circle(hs.geometry.rect(mouseScrollStartPos.x - mouseScrollCircleRad, mouseScrollStartPos.y - mouseScrollCircleRad, mouseScrollCircleRad * 2, mouseScrollCircleRad * 2))
+            mouseScrollCircle:setStrokeColor({["red"]=0.3, ["green"]=0.3, ["blue"]=0.3, ["alpha"]=1})
+            mouseScrollCircle:setFill(false)
+            mouseScrollCircle:setStrokeWidth(1)
+            mouseScrollCircle:show()
+        end
+
+        -- send scroll event if cursor moved more than circle's radius
+        if xDiff > mouseScrollCircleRad or yDiff > mouseScrollCircleRad then
+            -- get real xDiff and yDiff
+            deltaX = mouseScrollDragPosX - mouseScrollStartPos.x
+            deltaY = mouseScrollDragPosY - mouseScrollStartPos.y
+
+            -- use 'scrollSpeedMultiplier'
+            deltaX = deltaX * scrollSpeedMultiplier
+            deltaY = deltaY * scrollSpeedMultiplier
+
+            -- square for better scroll acceleration
+            if scrollSpeedSquareAcceleration then
+                -- mod to keep negative values
+                deltaXDirMod = 1
+                deltaYDirMod = 1
+
+                if deltaX < 0 then
+                    deltaXDirMod = -1
+                end
+                if deltaY < 0 then
+                    deltaYDirMod = -1
+                end
+
+                deltaX = deltaX * deltaX * deltaXDirMod
+                deltaY = deltaY * deltaY * deltaYDirMod
+            end
+
+            -- math.floor - scroll event accepts only integers
+            deltaX = math.floor(deltaX)
+            deltaY = math.floor(deltaY)
+
+            -- reverse Y scroll if 'reverseVerticalScrollDirection' set to true
+            if reverseVerticalScrollDirection then
+                deltaY = deltaY * -1
+            end
+
+            -- send scroll event
+            hs.eventtap.event.newScrollEvent({-deltaX, deltaY}, {}, 'pixel'):post()
+        end
+    end
+
+    -- restart timer
+    mouseScrollTimer = hs.timer.doAfter(mouseScrollTimerDelay, mouseScrollTimerFunction)
+end
+
+-- start override functions
+overrideScrollMouseDown:start()
+overrideScrollMouseUp:start()
+overrideScrollMouseDrag:start()
+
+------------------------------------------------------------------------------------------
+-- END OF AUTOSCROLL WITH MOUSE WHEEL BUTTON
+------------------------------------------------------------------------------------------
